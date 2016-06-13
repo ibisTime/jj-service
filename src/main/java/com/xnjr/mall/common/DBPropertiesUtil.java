18d828da30528bf5c7197243069b514304780fb9
haiqingzheng <leo.zheng@ommo.cn>
2016-06-13 09:42:10 +0800

    配置修改

--- src/main/java/com/xnjr/mall/common/DBPropertiesUtil.java
+++ src/main/java/com/xnjr/mall/common/DBPropertiesUtil.java
@@ -10,7 +10,8 @@ package com.xnjr.mall.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.xnjr.mall.bo.ISYSConfigBO;
+import com.xnjr.mall.ao.ISYSConfigAO;
+import com.xnjr.mall.spring.SpringContextHolder;
 
 /** 
  * @author: haiqingzheng 
@@ -20,19 +21,31 @@ import com.xnjr.mall.bo.ISYSConfigBO;
 public class DBPropertiesUtil {
 
     @Autowired
-    private static ISYSConfigBO sysConfigBO;
+    private static ISYSConfigAO sysConfigAO = SpringContextHolder
+        .getBean(ISYSConfigAO.class);
 
     public static final class Config {
 
         // 短信组件biz地址
-        public static String SMS_URL = sysConfigBO.getConfigValue("SMS_URL");
+        public static String SMS_URL = sysConfigAO.getConfigValue("SMS_URL");
 
         // 用户模块biz地址
-        public static String USER_URL = sysConfigBO.getConfigValue("USER_URL");
+        public static String USER_URL = sysConfigAO.getConfigValue("USER_URL");
 
         // 账户模块biz地址
-        public static String ACCOUNT_URL = sysConfigBO
+        public static String ACCOUNT_URL = sysConfigAO
             .getConfigValue("ACCOUNT_URL");
 
+        // 账户数据库名称
+        public static String ACCOUNT_DB = sysConfigAO
+            .getConfigValue("ACCOUNT_DB");
+
+        // 用户数据库名称
+        public static String USER_DB = sysConfigAO.getConfigValue("USER_DB");
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(DBPropertiesUtil.Config.SMS_URL);
     }
 }
